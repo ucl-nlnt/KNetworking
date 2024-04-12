3330697bf7c71cb1efc8739f840d6c273d641ac0
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-04-12 14:11:48 +0800

    init file structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nlnt-networking',
+    version='0.0.1',
+    description='private package for networking in UCL NLNT',
+    url='git@github.com:ucl-nlnt/KNetworking.git',
+    author='NLNT',
+    author_email='',
+    license='unlicense',
+    packages=['nlnt-networking'],
+    zip_safe=False
+)
